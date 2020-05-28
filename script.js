a049ca50b84fd57f198f719a4f5ef7caaa3549d1
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-05-28 03:28:46 -0400

    added placeholder images

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-// const navSlide = () => {
-//    const burger = document.querySelector('.burger');
-//    const nav = document.querySelector('.ul');
-
-//    burger.addEventListener('click', ()=>{
-//       nav.classList.toggle('.nav-links')
-//    })
-// }
-
-// const app = () => {
-//    navSlide()
-// } 
-
-// app()
-
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
