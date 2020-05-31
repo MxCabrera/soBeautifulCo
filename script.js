afdbabf83842ed3f8b8d34087641f75610edd6c8
MxCabrera <53961785+MxCabrera@users.noreply.github.com>
2020-05-30 23:02:07 -0400

    added array of services in script tag, was able to log the information in the console

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+const app = {};
+app.services = [
+   {
+      service: "Eyelash Extensions",
+      type: [
+         {
+            name: "Classic Lash Extensions",
+            price: 80.00,
+            duration: "1hr 45mins" 
+         }, {
+            name: "Hybrid Lashes",
+            price: 100.00,
+            duration: "2hrs"
+         }, {
+            name: "Volume",
+            price: 120.00,
+            duration: "2hr 30mins"
+         }
+      ]
+   }
+]
+
+app.getServices = () => {
+   console.log(app.services[0].type[0].name)
+}
+
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const links = document.querySelectorAll('.nav-links li');
@@ -9,3 +35,6 @@ hamburger.addEventListener('click', ()=> {
    })
 })
 
+$(function(){
+   app.getServices()
+})
